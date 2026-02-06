571169d733a0514b42bbd96f05ecbf5e7cbd05ee
TH Lee <lee@ssccs.org>
2026-02-06 15:46:55 +0100

    initialze ssccs.org

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="container" style={{ padding: '0' }}>
+      Welcome to ssccs.org - the official website of the SSCCS project. 
+    </main>
+  );
+}
