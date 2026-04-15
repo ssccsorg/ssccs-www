964f16d64369b94c45313fe15de32cc138647db7
Theo <theo@ssccs.org>
2026-04-16 00:28:21 +0200

    udate a link

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
         <span style={{ fontWeight: 'bold' }}>·</span>
 
         <span className={styles.group}>
-          <a href={appendDateQuery("https://ssccs.org/wp")}>Whitepaper</a> / <a href={appendDateQuery("https://ssccs.org/proposal")}>Proposal</a> 
+          <a href={appendDateQuery("https://ssccs.org/zenodo")}>Whitepaper</a> / <a href={appendDateQuery("https://ssccs.org/proposal")}>Proposal</a> 
         </span>
 
         <span style={{ fontWeight: 'bold' }}>·</span>
